dff96d77b5790a052068210351d1033ec7a4b746
Daniel Röwenstrunk <roewenstrunk@edirom.de>
2019-03-06 16:26:27 +0100

    Code structured and new features

---
+++ www/scripts/ds-app.js
@@ -0,0 +1,55 @@
+window.appData = {
+    "routes":[
+        {
+            "route": "tutti",
+            "title": "Tutti",
+            "mapPreview": "routes/tutti/map.png",
+            "geomap": "URL",
+            "length": "3 km",
+            "numStations": "7 Stationen",
+            "duration": "20/30 min",
+            "description": "<p><b>Überall die selbe alte Leier.</b> Die App läuft und der Text lässt auf sich warten.</p><p>Hier könnte eine schöne Beschreibung der Route stehen. Ein paar Angaben zu dem, was einen auf dem Spaziergang erwartet.</p>",
+            "stations": [
+                {
+                    "title": "Bahnhof",
+                    "geo-lat": 51.940479,
+                    "geo-lon": 8.873282,
+                    "img": "URL",
+                    "text": "<p><b>Der Detmolder Bahnhof</b> verfügt über ein in Nordrhein-Westfalen einzigartiges Empfangsgebäude. Heute lassen lediglich die Ornamente an der Sandsteinfassade die ursprüngliche Pracht des Empfangsgebäudes erahnen. Bis ins 20. Jahrhundert diente ein Teil, das sogenannte Fürstenzimmer, dem lippischen Fürsten als exklusiver Warte- und Empfangsbereich. Gegenüber dem ehemaligen Fürstenzimmer – an den Fahrradständern – wird seit einigen Jahren zu bestimmten Zeiten Musik abgespielt, wodurch sich die Gäste der Residenzstadt musikalisch begrüßt fühlen können. Und...? Hören Sie die Musik? Erkennen Sie das Stück?</p>",
+                    "source": "<p>Strohmann, Dirk. <em>Das Empfangsgebäude des Detmolder Bahnhofs und sein Fürstenzimmer</em>. Münster 2009.</p>",
+                    "text-audio": "URL",
+                    "audio-file": "URL",/*der Bahnhof hat keine Musik */
+                    "audio-title": "",
+                    "audio-composer": "",
+                    "audio-label": ""
+                }
+            ]
+        },
+        {
+            "route": "mezzo",
+            "title": "Mezzo",
+            "mapPreview": "routes/mezzo/map.png",
+            "geomap": "URL",
+            "length": "3 km",
+            "numStations": "7 Stationen",
+            "duration": "20/30 min",
+            "description": "<p><b>Überall die selbe alte Leier.</b> Die App läuft und der Text lässt auf sich warten.</p><p>Hier könnte eine schöne Beschreibung der Route stehen. Ein paar Angaben zu dem, was einen auf dem Spaziergang erwartet.</p>",
+            "stations": [
+                {
+                    "title": "Bahnhof",
+                    "geo-lat": 51.940479,
+                    "geo-lon": 8.873282,
+                    "img": "URL",
+                    "text": "<p><b>Der Detmolder Bahnhof</b> verfügt über ein in Nordrhein-Westfalen einzigartiges Empfangsgebäude. Heute lassen lediglich die Ornamente an der Sandsteinfassade die ursprüngliche Pracht des Empfangsgebäudes erahnen. Bis ins 20. Jahrhundert diente ein Teil, das sogenannte Fürstenzimmer, dem lippischen Fürsten als exklusiver Warte- und Empfangsbereich. Gegenüber dem ehemaligen Fürstenzimmer – an den Fahrradständern – wird seit einigen Jahren zu bestimmten Zeiten Musik abgespielt, wodurch sich die Gäste der Residenzstadt musikalisch begrüßt fühlen können. Und...? Hören Sie die Musik? Erkennen Sie das Stück?</p>",
+                    "source": "<p>Strohmann, Dirk. <em>Das Empfangsgebäude des Detmolder Bahnhofs und sein Fürstenzimmer</em>. Münster 2009.</p>",
+                    "text-audio": "URL",
+                    "audio-file": "URL",/*der Bahnhof hat keine Musik */
+                    "audio-title": "",
+                    "audio-composer": "",
+                    "audio-label": ""
+                }
+            ]
+        }
+    ]
+};
+
